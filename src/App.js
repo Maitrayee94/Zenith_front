0e87d77bbe5e2bb4926653e1388adf4df916b9cc
Maitrayee Das <maitrayeedas94@gmail.com>
2023-12-05 19:25:01 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,115 @@
+import { useState, useEffect } from "react";
+import { ethers } from "ethers";
+import "./App.css";
+import { CONTRACT_ABI, CONTRACT_ADDRESS } from "./constants/index.js";
+
+function App() {
+  const [account, setAccount] = useState("");
+  const [newValue, SetNewvalue] = useState("");
+  const [ address, setAddress] = useState("");
+  const provider = new ethers.providers.Web3Provider(window.ethereum);
+  const signer = provider.getSigner();
+  const contract = new ethers.Contract(CONTRACT_ADDRESS, CONTRACT_ABI, signer);
+  //const nonce = 5;
+ 
+  const connectMetamask = async (event) => {
+    event.preventDefault();
+    if (typeof window.ethereum !== "undefined") {
+      try {
+        const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+        setAccount(accounts[0]);
+        console.log(accounts[0]);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+  const getValue = async (event) => {
+    event.preventDefault();
+    try {
+      
+      console.log(contract.address);
+      
+      
+      const txResponse = await contract.getState();
+      
+      console.log(txResponse.toNumber());
+      window.alert("Current Value: " + txResponse.toNumber());
+    } catch (error) {
+      console.error(error);
+    }
+    
+  };
+
+  const UpdateNewValue = async (event) => {
+    event.preventDefault();
+    try {
+      const txResponse = await contract.updateState(newValue);
+      await txResponse.wait();
+      //console.log(txResponse);
+    } catch (error) {
+      //console.error(error);
+      window.alert("You are not whitelisted");
+    }
+    window.alert("Successfully Update the Value");
+  };
+
+  const whitelist = async (event) => {
+    event.preventDefault();
+    try {
+      const txResponse = await contract.updateWhitelist(address , true);
+      await txResponse.wait();
+      //console.log(txResponse);
+    } catch (error) {
+      //console.error(error);
+      window.alert("You are not Owner");
+    }
+    window.alert("Whitelist Address added");
+  };
+
+ 
+  return (
+    <div className="App">
+      <div className="desktop-1">
+      <div className="word-break-parent">
+      <form className="f1">
+      <p className="head">Zenith X</p>
+      <br />
+    
+        <button className="connect" onClick={connectMetamask}>
+          Connect Metamask
+        </button>
+       
+        <br></br>
+        <button className="lock" onClick={getValue}>
+          Current Value
+        </button>
+        <br></br>
+        <button className="withdraw" onClick={UpdateNewValue}>
+          Update
+        </button>
+        <input type="text" className="value" placeholder="Value" value={newValue} onChange={(e) => SetNewvalue(e.target.value)}></input>
+        <br></br>
+        <button className="withdraw" onClick={whitelist}>
+          Add Address
+        </button>
+        <input type="text" className="value" placeholder="Address" value={address} onChange={(e) => setAddress(e.target.value)}></input>
+        <br></br>
+
+        {account ==="" ? (
+      <p></p>
+): account !=="" ?(
+      <p className="account">Hello: {account}</p>
+      ): null}
+      </form>
+      
+      <br />
+     
+
+    </div>
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/constants/index.js
@@ -0,0 +1,124 @@
+export const CONTRACT_ADDRESS= "0xd5CEf8806aAEFdD474B6E37E52A6cf1810cBe4f0";
+export const CONTRACT_ABI=  [
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "newValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "StateUpdated",
+    "type": "event"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": true,
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      },
+      {
+        "indexed": false,
+        "internalType": "bool",
+        "name": "isWhitelisted",
+        "type": "bool"
+      }
+    ],
+    "name": "WhitelistUpdated",
+    "type": "event"
+  },
+  {
+    "inputs": [],
+    "name": "counter",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "getState",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "newValue",
+        "type": "uint256"
+      }
+    ],
+    "name": "updateState",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "account",
+        "type": "address"
+      },
+      {
+        "internalType": "bool",
+        "name": "isWhitelisted",
+        "type": "bool"
+      }
+    ],
+    "name": "updateWhitelist",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "name": "whitelist",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
